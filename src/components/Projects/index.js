dac2f0125b41efb5434ed8f690e8ba094d8f60fe
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-05-04 20:45:24 -0300

    Listagem de projetos

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,55 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import ProjectsActions from 'store/ducks/projects';
 
 import Button from 'styles/components/Button';
 
 import { Container, Project } from './styles';
 
-const Projects = ({ activeTeam }) => {
-  if (!activeTeam) return null;
+class Projects extends Component {
+  static propTypes = {
+    getProjectsRequest: PropTypes.func.isRequired,
+    activeTeam: PropTypes.shape({
+      name: PropTypes.string,
+    }).isRequired,
+    projects: PropTypes.shape({
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          title: PropTypes.string,
+        }),
+      ),
+    }).isRequired,
+  };
 
-  return (
-    <Container>
-      <header>
-        <h1>{activeTeam.name}</h1>
-        <div>
-          <Button onClick={() => {}}>+ Novo</Button>
-          <Button onClick={() => {}}>Membros</Button>
-        </div>
-      </header>
+  componentDidMount() {
+    const { activeTeam, getProjectsRequest } = this.props;
 
-      <Project>
-        <p>Aplicação com React Native</p>
-      </Project>
-      <Project>
-        <p>Aplicação com React Native</p>
-      </Project>
-      <Project>
-        <p>Aplicação com React Native</p>
-      </Project>
-      <Project>
-        <p>Aplicação com React Native</p>
-      </Project>
-      <Project>
-        <p>Aplicação com React Native</p>
-      </Project>
-      <Project>
-        <p>Aplicação com React Native</p>
-      </Project>
-    </Container>
-  );
-};
+    if (activeTeam) {
+      getProjectsRequest();
+    }
+  }
 
-Projects.propTypes = {
-  activeTeam: PropTypes.shape({
-    name: PropTypes.string,
-  }).isRequired,
-};
+  render() {
+    const { activeTeam, projects } = this.props;
+
+    if (!activeTeam) return null;
+
+    return (
+      <Container>
+        <header>
+          <h1>{activeTeam.name}</h1>
+          <div>
+            <Button onClick={() => {}}>+ Novo</Button>
+            <Button onClick={() => {}}>Membros</Button>
+          </div>
+        </header>
+
+        {projects.data.map(project => (
+          <Project key={project.id}>
+            <p>{project.title}</p>
+          </Project>
+        ))}
+      </Container>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   activeTeam: state.teams.active,
+  projects: state.projects,
 });
 
-export default connect(mapStateToProps)(Projects);
+const mapDispatchToProps = dispatch => bindActionCreators(ProjectsActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Projects);
--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,14 +1,15 @@
-import { combineReducers } from "redux";
-import { connectRouter } from "connected-react-router";
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
 
-import { reducer as toastr } from "react-redux-toastr";
-import { reducer as auth } from "./auth";
-import { reducer as teams } from "./teams";
+import { reducer as toastr } from 'react-redux-toastr';
+import { reducer as auth } from './auth';
+import { reducer as teams } from './teams';
+import { reducer as projects } from './projects';
 
-export default history =>
-  combineReducers({
-    auth,
-    toastr,
-    router: connectRouter(history),
-    teams
-  });
+export default history => combineReducers({
+  auth,
+  toastr,
+  router: connectRouter(history),
+  teams,
+  projects,
+});
