52e3d32abfd7f50e07258a9062081159870a1e20
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-05-04 20:01:47 -0300

    Layout de projetos + alterações no .eslintrc.json

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import TeamSwitcher from "components/TeamSwitcher";
+import TeamSwitcher from 'components/TeamSwitcher';
+import Projects from 'components/Projects';
 
-import { Container } from "./styles";
+import { Container } from './styles';
 
 const Main = () => (
   <Container>
     <TeamSwitcher />
+    <Projects />
   </Container>
 );
 
